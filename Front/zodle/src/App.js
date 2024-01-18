a25f72d2008c73d3d16b4d3680660927902165ef
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2024-01-19 01:37:31 +0200

    Add code text field

--- Front/zodle/src/App.js
+++ Front/zodle/src/App.js
@@ -1,24 +1,21 @@
 import { Button, IconButton, List, ListItem, ListItemButton, ListItemText, TextField, styled } from '@mui/material';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Add, Delete } from '@mui/icons-material';
-import { grey, purple } from '@mui/material/colors';
 import AddFileModal from './AddFileModal';
+import axios from 'axios';
+import CodeEditor from '@uiw/react-textarea-code-editor';
 
 //https://legacy.reactjs.org/docs/forms.html
 
-class DocEntry {
-  constructor(id) {
+class Script {
+  constructor(id, name, description, details, tags, script) {
     this.Id = id;
-    this.Title = 'Title ' + id;
-    this.Description = 'Description ' + id;
-    this.Details = 'Details ' + id;
-    this.Tags = ["Tag1"];
-
-    for(let i = 0; i < id; i++)
-    {
-      this.Details += "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-    }
+    this.Name = name;
+    this.Description = description;
+    this.Details = details;
+    this.Tags = tags;
+    this.Script = script;
   }
 }
 
@@ -27,20 +24,30 @@ function App() {
   const [selectedEntry, setSelectedEntry] = useState(null);
   const [isModalOpen, setModalOpen] = useState(false);
   const [searchField, setSearchField] = useState("");
+  const [scriptList, setScriptList] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://localhost:7109/ScriptStore')
+      .then(response => {
+        setScriptList(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
 
   const handleSearchFieldChange = (value) => {
     setSearchField(value.toLowerCase());
   };
 
-  const docList = [];
   for(let i = 0; i < 50; i++)
   {
-    docList.push(new DocEntry(i));
+    //scriptList.push(new DocEntry(i));
   }
-  const filteredList = docList.filter(
+  const filteredList = scriptList.filter(
     (script) => {
       return (
-        script.Title.toLowerCase().includes(searchField) ||
+        script.Name.toLowerCase().includes(searchField) ||
         script.Description.toLowerCase().includes(searchField) ||
         script.Tags.includes(searchField)
       );
@@ -88,7 +95,7 @@ function App() {
               {filteredList.map((entry) => 
                 <ListItem disablePadding>
                     <ListItemButton key={1} onClick={() => setSelectedEntry(entry)}>
-                      <ListItemText primary={entry.Title} />
+                      <ListItemText primary={entry.Name} />
                     </ListItemButton>
                 </ListItem>
               )}
@@ -102,7 +109,12 @@ function App() {
             {selectedEntry ? selectedEntry.Description : 'Cia turetu buti aprasymas'}
           </div>
           <div className='Details'>
-            {selectedEntry ? selectedEntry.Details : 'Cia turetu buti details'}
+            <CodeEditor
+              multiline
+              language='cs'
+              readOnly
+              value={selectedEntry ? selectedEntry.Script : 'Cia turetu buti details'}
+            />
           </div>
         </div>
       </div>
