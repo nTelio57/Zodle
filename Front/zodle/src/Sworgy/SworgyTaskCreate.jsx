34e7526b57ce51f5226b395301a7264168b61396
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2024-01-22 19:16:04 +0200

    Update task creation textfield size

--- Front/zodle/src/Sworgy/SworgyTaskCreate.jsx
+++ Front/zodle/src/Sworgy/SworgyTaskCreate.jsx
@@ -7,7 +7,7 @@ export default function SworgyTaskCreate({onTaskCreated}) {
     return (
         <Box sx={{ height: "100%", width: "100%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
             <Box sx={{ height: "60%", width: "80%"}}>
-                <TextField multiline onChange={(change) => setTask(change.target.value)} placeholder="Create your task for someone ;)" rows={22} fullWidth style={{ backgroundColor: "white", borderRadius: "4px"}}></TextField>
+                <TextField multiline onChange={(change) => setTask(change.target.value)} placeholder="Create your task for someone ;)" rows={15} fullWidth style={{ backgroundColor: "white", borderRadius: "4px"}}></TextField>
             </Box>
             <Button variant="filled" onClick={() => onTaskCreated(task)} style={{backgroundColor: "white", color: "#fc3d70", fontWeight: "bold", marginTop: "8px", width: "50%"}}>Create</Button>
         </Box>
